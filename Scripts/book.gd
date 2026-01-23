808dcd6aec5158a74db07b24fad47a6e4c0ea258
Jordan Barber <jordanb421@outlook.com>
2026-01-23 23:28:47 +0000

    Creating table and interactable book assets

---
+++ Scripts/book.gd
@@ -0,0 +1,9 @@
+extends Interactable
+## Controls the interaction pickup and display of the book
+
+func get_interaction_text() -> String:
+	return interaction_text
+	
+func interact(_interactor: Node) -> void:
+	pass
+	
