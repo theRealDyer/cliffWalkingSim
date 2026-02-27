d1dfd5e416770442be52ed3a35979401b22e3ebe
theRealDyer <54688154+theRealDyer@users.noreply.github.com>
2026-02-27 16:59:47 +0000

    - increased default project resolution to 1080 - speed is now walking speed, hold shift to run - hid player mesh (we don't need it) - reduced default/scripted mouse sensitivity.

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -6,9 +6,10 @@ const MAX_PITCH := deg_to_rad(80)
 @export var interaction_ray: RayCast3D
 @export var prompt_ui: InteractablePromptUI
 @export_group("Movement")
-@export var speed := 5.0
+@export var walk_speed := 2.0
+@export var run_speed := 8.0
 @export var jump_velocity := 4.5
-@export var mouse_sensitivity := 0.005
+@export var mouse_sensitivity := 0.001
 
 # Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -44,12 +45,13 @@ func _physics_process(delta: float) -> void:
 	neck_basis = neck_basis.orthonormalized()
 
 	var direction = (neck_basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var current_speed := run_speed if Input.is_action_pressed("run") else walk_speed
 	if direction:
-		velocity.x = direction.x * speed
-		velocity.z = direction.z * speed
+		velocity.x = direction.x * current_speed
+		velocity.z = direction.z * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
-		velocity.z = move_toward(velocity.z, 0, speed)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
+		velocity.z = move_toward(velocity.z, 0, current_speed)
 
 	move_and_slide()
 
